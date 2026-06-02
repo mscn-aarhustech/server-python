6dc76442320a780433a1a3291b10db7447765536
Michael Schmidt Nissen <mscn@aarhustech.dk>
2026-06-02 14:02:08 +0200

    updated python script

--- HelloWorldKubernetes/HelloWorld.py
+++ HelloWorldKubernetes/HelloWorld.py
@@ -3,6 +3,6 @@ import time
 i = 0
 
 while True:
-    print("Hello, World from Kubernetes! # " + str(i))
+    print("Hello, World from Kubernetes! # " + str(i), flush=True)
     i = i+1
     time.sleep(1.0)
